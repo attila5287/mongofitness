6d64f854159b98711bae5268789a222200555084
attila5287 <atiturkoz@hotmail.com>
2021-04-25 17:30:12 -0600

    heroku run

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 const router = require( 'express' ).Router();
 // const Transaction = require( "../models/Transaction" );
-const demo = require( '../demo' );
+// const seeders = require( '../seeders/seed.js' );
+
+router.get('/', (req, res) => {
+  res.send( '<h1>Hello World</h1>' );
+
+});
+
+
+
+module.exports = router;
