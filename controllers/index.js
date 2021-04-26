1feca5d719a949be8bacd1ac78fd27234d23bb8d
attila5287 <atiturkoz@hotmail.com>
2021-04-26 05:51:31 -0600

    exercise menu on landing

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require( 'express' ).Router();
+const {gen_imgs} = require( '../utils/helpers' );
 // const Workout = require( "../models/Workout" );
 
 router.get('/', (req, res) => {
-
-  res.render( 'dashboard' );
+  console.log('helpers.gen_imgs :>> ', gen_imgs);
+  res.render( 'landing', { images: gen_imgs } );
+  
 
 });
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,13 @@
 module.exports = {
-  calculate_progress: (need, coll) => {
-    return Math.floor((coll / need) * 100);
+  calculate_progress: ( need, coll ) => {
+    return Math.floor( ( coll / need ) * 100 );
   },
-  format_amount: (amount) => {
+  format_amount: ( amount ) => {
     // format large numbers with commas
     return parseInt( amount ).toLocaleString();
-    
+
   },
-  format_date: (date) => {
+  format_date: ( date ) => {
     // Using JavaScript Date methods, we get and format the month, date, and year
     // We need to add one to the month since it is returned as a zero-based value
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
@@ -15,4 +15,18 @@ module.exports = {
       new Date(date).getFullYear() + 5
     }`;
   },
+  gen_imgs: [
+    "aerobic",
+    "dance",
+    "explosive",
+    "non_aerobic",
+    "static_str",
+    "yoga",
+    "ball",
+    "dynamic_str",
+    "mat",
+    "resistance",
+    "weight",
+    "zumba",
+  ]
 };
