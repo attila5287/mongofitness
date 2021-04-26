3535a0dda23511acd21b4172dd3718f66b530fc9
attila5287 <atiturkoz@hotmail.com>
2021-04-26 10:27:11 -0600

    animate

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 const router = require( 'express' ).Router();
 const {burning, lifting} = require( '../utils/helpers' );
 // const Workout = require( "../models/Workout" );
+// router.use('/api', apiRoutes);
+
 
 router.get( '/', ( req, res ) => {
   
   res.render( 'landing', { burning: burning, lifting:lifting  } );
-  
 
 });
 
