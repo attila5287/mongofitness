82a3a01fa446c562a6e4ed5322ffccff407d2038
attila5287 <atiturkoz@hotmail.com>
2021-04-26 09:03:03 -0600

    collapse folder added

---
+++ public/js/collapse/calorie_div.js
@@ -0,0 +1,18 @@
+
+let collapse_calorie_collapsed = false;
+$( '#collapse_calorie_button' )
+  .on( "click", () => {
+    if ( collapse_calorie_collapsed ) {
+      collapse_calorie_collapsed = false;
+      $( "#collapse_calorie_text" ).text( "Hide" );
+      $( "#collapse_calorie_icon" ).removeClass( "fa-chevron-up" );
+      $( "#collapse_calorie_icon" ).addClass( "fa-chevron-down" );
+    } else {
+      collapse_calorie_collapsed = true;
+      $( "#collapse_calorie_text" ).text( "Show" );
+      $( "#collapse_calorie_icon" ).removeClass( "fa-chevron-down" );
+      $( "#collapse_calorie_icon" ).addClass( "fa-chevron-up" );
+    }
+
+
+  } );
