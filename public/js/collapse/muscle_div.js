25907c927fed0d598c0ab0634e89c2763b3ab063
attila5287 <atiturkoz@hotmail.com>
2021-04-26 10:50:49 -0600

    push DU icopn

--- public/js/collapse/muscle_div.js
+++ public/js/collapse/muscle_div.js
@@ -3,11 +3,11 @@ $( '#collapse_muscle_button' )
   .on( "click", () => {
     if ( collapse_muscle_collapsed ) {
       collapse_muscle_collapsed = false;
-      $( "#collapse_muscle_text" ).text( "Hide" );
       $( "#collapse_muscle_icon" ).removeClass( "fa-chevron-up" );
       $( "#collapse_muscle_icon" ).addClass( "fa-chevron-down" );
     } else {
       collapse_muscle_collapsed = true;
+      $( "#collapse_muscle_text" ).text( "Hide" );
       $( "#collapse_muscle_text" ).text( "Show" );
       $( "#collapse_muscle_icon" ).removeClass( "fa-chevron-down" );
       $( "#collapse_muscle_icon" ).addClass( "fa-chevron-up" );
