74b3da94b5f9deac8aaaae9494c9e04f23c57055
attila5287 <atiturkoz@hotmail.com>
2021-04-29 03:31:59 -0600

    render test p

--- public/js/workouts/latest.js
+++ public/js/workouts/latest.js
@@ -1,20 +1,28 @@
-const response = await fetch('/api/work/latest', {
-  method: 'GET',
-  headers: { 'Content-Type': 'application/json' },
-} );
+const render_latest = async () => {
+  const response = await fetch('/api/work/latest', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }).catch(e => console.log(e));
 
+  if ( response.ok )
+  {
+    const json = await response.json();
+    console.log('json :>> ', json);
+    json.forEach( ( workout ) => {
+      workout.exercises.forEach( ex => {
+      Object.keys(ex).forEach(k => {
+        const p = $( '<p>' );
+        p.text( k + ':' + ex[ k ] );
+        $( '#workout_div' ).append( p );
+        
+      });
+    });
 
-if ( response.ok )
-{
-  response.exercises.forEach( ex => {
-    
-    
-    // $('#workout_div')
-  });
+    });
+  } else
+  {
+    console.log('error :>> ', response.errorCode);
+  }
 
-
-
-
-
-  
-}
+};
+render_latest();
