d9d5da1022bfe60edffa74be632cc24b32747811
attila5287 <atiturkoz@hotmail.com>
2021-04-30 00:26:04 -0600

    breadcrumb design

--- public/js/workouts/latest.js
+++ public/js/workouts/latest.js
@@ -1,38 +1,46 @@
 const render_latest = async () => {
-  const response = await fetch('/api/work/latest', {
+  const response = await fetch( '/api/work/latest', {
     method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  }).catch(e => console.log(e));
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  } ).catch( e => console.log( e ) );
 
-  if ( response.ok )
-  {
+  if ( response.ok ) {
     const json = await response.json();
-    
-    // console.log( 'json :>> ', json );
-    
-    json.forEach( ( workout ) => {
-      
-      // console.log( 'json._id :>> ', workout._id );
-      
-      const workout_id = $( '#latest_workout_id' )
-        .attr('data-id', workout._id)
-        .text( workout._id );
-      
-        workout.exercises.forEach( ex => {
-          const li = $( '<li>' );
-          li.attr('class', 'list-group-item bg-primary');
-          let text = '';
-        ['name','weight','reps','sets'].forEach(k => {
-          text = text + k + ':' + ex[ k ] + '- ';
-          li.text(text)
-        });
-        $( '#workout_div' ).append( li );
-    });
 
-    });
-  } else
-  {
-    console.log('error :>> ', response.errorCode);
+    // console.log( 'json :>> ', json );
+
+    json.forEach( ( workout ) => {
+
+      // console.log( 'json._id :>> ', workout._id );
+
+      const workout_id = $( '#latest_workout_id' )
+        .attr( 'data-id', workout._id )
+        .text( workout._id );
+
+        
+        workout.exercises.forEach( ex => {
+        const ol = $( '<ol>' );
+        ol.attr( 'class', 'breadcrumb' );
+        
+        [ 'name', 'weight', 'reps', 'sets' ].forEach( k => {
+
+          const li = $( '<li>' );
+          li.attr( 'class', 'breadcrumb-item active' );
+
+          li.text( k + ':' + ex[ k ] );
+          ol.append( li );
+
+        } );
+        
+        $( '#workout_div' ).append( ol );
+      } );
+        
+
+    } );
+  } else {
+    console.log( 'error :>> ', response.errorCode );
   }
 
 };
