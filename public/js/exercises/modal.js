6e5ba49a2aa1aec25b6e4e437f8c97c434481bdf
attila5287 <atiturkoz@hotmail.com>
2021-05-01 09:55:46 -0600

    add cardio to workout

---
+++ public/js/exercises/modal.js
@@ -0,0 +1,35 @@
+$('.cardio_forms').submit(async (event)=> {
+  event.preventDefault();
+  const id = event.target.dataset[ 'id' ];
+  console.log( 'id :>> ', id );
+  
+  const modal = '#' + id + '_modal';
+  const workout_id = $('#latest_workout_id').attr('data-id');
+  $( modal ).modal( 'hide' );
+  
+  console.log(workout_id);
+  
+  const input = {
+    type: "cardio",
+    name: $('#name_'+id).val(),
+    duration: +$('#duration_'+id).val(),
+    distance: +$('#distance_'+id).val(),
+  };
+  console.log('input :>> ', input);
+  
+  $( modal ).on( 'hidden.bs.modal', async function ( e ) {
+
+    const res = await fetch('/api/work/' + workout_id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(input),
+    });
+    if (res.ok) {
+      render_latest();
+      const toast = document.querySelector( '#toast' );
+      toast.classList.add('success');
+    }
+    
+  });
+
+});
--- public/js/workouts/render.js
+++ public/js/workouts/render.js
@@ -1,61 +1,96 @@
 const render_latest = async () => {
-  const response = await fetch('/api/work/latest', {
+  const response = await fetch( '/api/work/latest', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-  }).catch((e) => console.log(e));
+  } ).catch( ( e ) => console.log( e ) );
 
-  if (response.ok) {
-    $('#workout_div').empty();
-    
+  if ( response.ok ) {
+    $( '#workout_div' ).empty();
+
     const json = await response.json();
 
-    json.forEach((workout) => {
+    json.forEach( ( workout ) => {
       // console.log( 'json._id :>> ', workout._id );
 
-      const workout_id = $('#latest_workout_id')
-        .attr('data-id', workout._id)
-        .text(workout._id);
+      const workout_id = $( '#latest_workout_id' )
+        .attr( 'data-id', workout._id )
+        .text( workout._id );
 
-      workout.exercises.forEach((ex) => {
+      let ex_index =0;
+      workout.exercises.forEach( ( ex ) => {
+        ex_index ++;
+        let ol = null;
+        const icons = {
+          resistance: 'dumbbell',
+          cardio: 'bicycle',
+        };
 
-        const ol = $('<ol>');
-        ol.attr('class', 'breadcrumb bg-info text-capitalize');
-
-        ['name', 'weight', 'reps', 'sets'].forEach((k) => {
-          const li = $('<li>');
-          li.attr('class', 'breadcrumb-item active');
-
-          li.text(k + ': ' + ex[k]);
-          ol.append(li);
-        });
+        const styles = {
+          resistance: 'primary',
+          cardio: 'secondary',
+        };
+        const anime = {
+          0: 'animated fadeInLeft',
+          1: 'animated fadeInRight',
+        };
+        const displayed = {
+          resistance: ['name', 'weight', 'reps', 'sets'],
+          cardio: ['name', 'duration', 'distance', ],
+        };
         
-        const btn = $('<button>');
-        btn.attr('class', 'btn btn-sm btn-secondary ml-5');
-        
-        btn.text( 'delete' );
-        $(ol).append(btn);
-        $('#workout_div').append(ol);
-              
-        btn.on('click', async function delete_btn_handler() {
-          console.log('delete btn handler');
+        ol = $( '<ol>' );
+          ol.attr(
+            'class',
+            `breadcrumb bg-${styles[ex.type]} ${
+              anime[ex_index % 2]
+            } text-capitalize align-items-center shadow-lg py-0`
+          );
+          const icon_li = $( '<li>' );
+        const icon = $( '<h1>' );
 
-          const del_resp = await fetch('/api/work/delete/' + workout._id, {
+          icon_li.attr('class', 'breadcrumb-item active');
+          icon.attr('class', 'text-light fas fa-' + icons[ex.type]);
+          icon_li.append(icon);
+          ol.append(icon_li);
+          
+          displayed[ex.type].forEach( ( k ) => {
+            const li = $( '<li>' );
+            li.attr( 'class', 'breadcrumb-item active' );
+
+            li.text( k + ': ' + ex[ k ] );
+            ol.append( li );
+          } );
+
+          const btn_li = $('<li>');
+          const btn = $( '<button>' );
+        btn.attr( 'class', 'btn btn-outline-light border-0 ml-5 shadow-lg' );
+
+        btn.html( '<h5 class="fas fa-times-circle text-light">  </h5>' );
+        $( ol ).append( btn );
+        $( '#workout_div' ).append( ol );
+
+        btn.on( 'click', async function delete_btn_handler() {
+          console.log( 'delete btn handler' );
+
+          const del_resp = await fetch( '/api/work/delete/' + workout._id, {
             method: 'PUT',
-            body: JSON.stringify({ name: ex.name }),
+            body: JSON.stringify( {
+              name: ex.name
+            } ),
             headers: {
               'Content-Type': 'application/json',
             },
           } ).catch( ( e ) => console.log( e ) );
-          if (del_resp.ok) {
+          if ( del_resp.ok ) {
             render_latest();
           }
-        });
+        } );
       } );
-    });
+    } );
   } else {
-    console.log('error :>> ', response.errorCode);
+    console.log( 'error :>> ', response.errorCode );
   }
 };
 render_latest();
